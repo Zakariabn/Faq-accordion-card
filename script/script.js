d217c2cc9034e3b943c577b79eecff0a9f5ce485
Zakaria <zakaria7262@gmail.com>
2021-11-05 17:02:32 +0600

    first commit

---
+++ script/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// let dropDownIcon = document.querySelector(".dorpdown-icon");
+// let question = document.querySelector(".qu");
+// let answer = document.querySelector(".ans");
+
+// function display () {
+//    dropDownIcon.style.transform = 'rotateX(0deg)';
+//    dropDownIcon.style.transition = "all .3s ease";
+//    answer.style.display = 'unset';
+//    answer.style.transition = "all .2s ease";
+//    question.style.fontWeight = "bolder";
+//    question.style.fontSize = "14px";
+//    question.style.transition = "all .3s ease";
+//    return display;
+// }
